7656b0daf236e8f225ac3be1c20df18a55cc50de
Masaki Kobayashi <contact@mkobayashi.me>
2021-02-27 18:38:09 +0900

    eslint-disable in custom app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../../styles/globals.css"
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, react/prop-types
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
