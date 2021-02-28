b9c5c7e1b0ad8dfe8162e31f0e5a39af274a3c1c
Masaki Kobayashi <contact@mkobayashi.me>
2021-02-28 20:37:52 +0900

    Move styles dir

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from "react"
 
 import type { AppProps } from "next/app"
 
-import "../../styles/globals.css"
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />
