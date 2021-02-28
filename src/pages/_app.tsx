f221a6932485cea3fa877c09b06a3a7f169f8136
Masaki Kobayashi <contact@mkobayashi.me>
2021-02-28 21:31:29 +0900

    Add global html head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import { ReactElement } from "react"
 
 import type { AppProps } from "next/app"
+import Head from "next/head"
 
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <title key="title">雙峰祭オンラインシステム</title>
+        <meta name="robots" content="noindex" key="robots" />
+        <meta name="googlebot" content="noindex" key="googlebot" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
