435bde13bc9e80713ce7bb801b0d91dd7f771dbc
Masaki Kobayashi <contact@mkobayashi.me>
2021-03-07 01:35:42 +0900

    Add story for Spinner

---
+++ src/components/Spinner/index.stories.tsx
@@ -0,0 +1,31 @@
+import { Story } from "@storybook/react"
+
+import { Spinner } from "."
+
+export default {
+  title: Spinner.name,
+  component: Spinner,
+}
+
+export const Index: Story<Spinner.Props> = ({
+  color,
+  radius,
+  lineWidth,
+}: Spinner.Props) => {
+  return <Spinner {...{ color, radius, lineWidth }} />
+}
+
+Index.argTypes = {
+  color: {
+    control: { type: "color" },
+    defaultValue: "#000",
+  },
+  radius: {
+    control: { type: "range", min: 10, max: 50 },
+    defaultValue: 28,
+  },
+  lineWidth: {
+    control: { type: "range", min: 1, max: 5 },
+    defaultValue: 2,
+  },
+}
