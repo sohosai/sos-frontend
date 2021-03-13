7b5bf1c8dc6b7e2ee3262801e3f739ff8d47ce1a
Masaki Kobayashi <contact@mkobayashi.me>
2021-03-13 18:09:10 +0900

    Add User model

---
+++ src/types/models/user/index.ts
@@ -0,0 +1,28 @@
+export type User = Readonly<{
+  id: UserId
+  created_at: Date
+  name: UserName
+  kana_name: UserKanaName
+  email: string
+  phone_number: string
+  affiliation: string
+  role: UserRole
+}>
+
+export type UserId = string
+
+export type UserName = Readonly<{
+  first: string
+  last: string
+}>
+
+export type UserKanaName = Readonly<{
+  first: string
+  last: string
+}>
+
+export type UserRole =
+  | "administrator"
+  | "committee_operator"
+  | "committee"
+  | "general"
