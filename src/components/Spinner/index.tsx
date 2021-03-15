03c1a31453a1747ea90882195e641af03cbca964
Masaki Kobayashi <contact@mkobayashi.me>
2021-03-15 14:10:06 +0900

    Define color variation of Spinner

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -5,21 +5,15 @@ import styles from "./index.module.scss"
 declare namespace Spinner {
   type Props = Readonly<
     Partial<{
-      color: string
+      color: "gray" | "white" | "brand"
       size: "sm" | "md"
     }>
   >
 }
 
-const Spinner: FC<Spinner.Props> = ({ color = "#000", size = "md" }) => {
+const Spinner: FC<Spinner.Props> = ({ color = "gray", size = "md" }) => {
   return (
-    <div
-      className={styles.wrapper}
-      style={{
-        ["--spinner-color" as any]: color,
-      }}
-      data-size={size}
-    >
+    <div className={styles.wrapper} data-size={size} data-color={color}>
       <svg className={styles.circular}>
         <circle className={styles.path} pathLength="100" />
       </svg>
