9e259b8e4e18ee6218208b4a3a558c7a1b7ae67c
Masaki Kobayashi <contact@mkobayashi.me>
2021-03-22 02:08:18 +0900

    Fix the wrong impl of lang attribute of html element

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,7 +25,6 @@ function MyApp({
   return (
     <>
       <Head>
-        <html lang="ja" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
