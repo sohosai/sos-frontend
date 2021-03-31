acc6ff3cfac72683943936a15caeabfbceee779f
Masaki Kobayashi <contact@mkobayashi.me>
2021-03-31 23:44:37 +0900

    Refactor menubar links

--- src/layouts/sidebar/links.tsx
+++ src/layouts/sidebar/links.tsx
@@ -1,41 +1,39 @@
 import { FC } from "react"
 
 import Link from "next/link"
-import { useRouter, NextRouter } from "next/router"
+import { useRouter } from "next/router"
 import { PageOptions } from "next"
 
 import styles from "./links.module.scss"
 
-type Links = {
-  [layoutName in PageOptions["layout"]]?: {
-    href: string
-    title: string
-    active: (router?: NextRouter) => boolean
-  }[]
-}
-
-const links: Links = {
-  default: [
-    {
-      href: "/",
-      title: "トップページ",
-      active: (router) => router.pathname === "/",
-    },
-    {
-      href: "/login",
-      title: "ログイン",
-      active: (router) => router.pathname === "/login",
-    },
-    {
-      href: "/signup",
-      title: "アカウント登録",
-      active: (router) => router.pathname === "/signup",
-    },
-  ],
-}
-
 export const Links: FC<Pick<PageOptions, "layout">> = ({ layout }) => {
   const router = useRouter()
+
+  const links: {
+    [layoutName in PageOptions["layout"]]?: Array<{
+      href: string
+      title: string
+      active: () => boolean
+    }>
+  } = {
+    default: [
+      {
+        href: "/",
+        title: "トップページ",
+        active: () => router.pathname === "/",
+      },
+      {
+        href: "/login",
+        title: "ログイン",
+        active: () => router.pathname === "/login",
+      },
+      {
+        href: "/signup",
+        title: "アカウント登録",
+        active: () => router.pathname === "/signup",
+      },
+    ],
+  }
 
   return (
     <menu className={styles.wrapper}>
@@ -43,7 +41,7 @@ export const Links: FC<Pick<PageOptions, "layout">> = ({ layout }) => {
         {links[layout].map(({ href, title, active }, index) => (
           <li key={index}>
             <Link href={href}>
-              <a className={styles.link} data-active={active(router)}>
+              <a className={styles.link} data-active={active()}>
                 <p className={styles.label}>{title}</p>
               </a>
             </Link>
