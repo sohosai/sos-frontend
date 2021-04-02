dd8e79a32b78ec951af422e7368bc775e32769ad
Masaki Kobayashi <contact@mkobayashi.me>
2021-04-02 17:50:27 +0900

    Use icons in the menubar

--- src/layouts/sidebar/links.tsx
+++ src/layouts/sidebar/links.tsx
@@ -16,6 +16,7 @@ export const Links: FC<Pick<PageOptions, "layout">> = ({ layout }) => {
     [layoutName in PageOptions["layout"]]?: Array<{
       href: string
       title: string
+      icon: string
       visible: () => boolean
       active: () => boolean
     }>
@@ -24,24 +25,28 @@ export const Links: FC<Pick<PageOptions, "layout">> = ({ layout }) => {
       {
         href: "/",
         title: "トップページ",
+        icon: "home",
         visible: () => true,
         active: () => router.pathname === "/",
       },
       {
         href: "/login",
         title: "ログイン",
+        icon: "log-in",
         visible: () => !firebaseUser,
         active: () => router.pathname === "/login",
       },
       {
         href: "/signup",
         title: "アカウント登録",
+        icon: "user-plus",
         visible: () => !firebaseUser,
         active: () => router.pathname === "/signup",
       },
       {
         href: "/init",
         title: "アカウント情報登録",
+        icon: "user-circle",
         visible: () => Boolean(firebaseUser) && !sosUser,
         active: () => router.pathname === "/init",
       },
@@ -51,13 +56,14 @@ export const Links: FC<Pick<PageOptions, "layout">> = ({ layout }) => {
   return (
     <menu className={styles.wrapper}>
       <ul className={styles.links}>
-        {links[layout].map(({ href, title, visible, active }, index) => {
+        {links[layout].map(({ href, title, icon, visible, active }, index) => {
           if (!visible()) return
 
           return (
             <li key={index}>
               <Link href={href}>
                 <a className={styles.link} data-active={active()}>
+                  <i className={`jam-icon jam-${icon} ${styles.linkIcon}`} />
                   <p className={styles.label}>{title}</p>
                 </a>
               </Link>
