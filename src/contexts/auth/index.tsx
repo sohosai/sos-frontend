b47368e0cac32cc58ac94ee0f32afcc4e7048da3
Masaki Kobayashi <contact@mkobayashi.me>
2021-04-13 16:27:20 +0900

    Rename a env var to meet Firebase

--- src/contexts/auth/index.tsx
+++ src/contexts/auth/index.tsx
@@ -18,7 +18,7 @@ import { FullScreenLoading } from "../../foundations/fullScreenLoading"
 if (!firebase.apps.length) {
   firebase.initializeApp({
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_API_AUTH_DOMAIN,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     appID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   })
