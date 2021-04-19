956afac7edbccaceaef1ff524df8543b43f3a540
Masaki Kobayashi <contact@mkobayashi.me>
2021-04-20 01:47:47 +0900

    Impl listProjectForms api handler

---
+++ src/lib/api/form/listProjectForms.ts
@@ -0,0 +1,27 @@
+import { client } from "../client"
+
+import type { Form } from "../../../types/models/form/"
+
+declare namespace listProjectForms {
+  type Props = Readonly<{
+    props: {
+      project_id: string
+    }
+    idToken: string
+  }>
+}
+
+const listProjectForms = async ({
+  props: { project_id },
+  idToken,
+}: listProjectForms.Props): Promise<{ forms: Form[] }> => {
+  return client({ idToken })
+    .get("project/form/list", {
+      searchParams: {
+        project_id,
+      },
+    })
+    .json()
+}
+
+export { listProjectForms }
