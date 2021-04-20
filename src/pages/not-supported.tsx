7239c7f90025e49473e1e0e4686a1e033b03f76a
Masaki Kobayashi <contact@mkobayashi.me>
2021-04-20 15:49:02 +0900

    Style refinements in /not-supported

--- src/pages/not-supported.tsx
+++ src/pages/not-supported.tsx
@@ -1,6 +1,8 @@
 import { PageFC } from "next"
 import Head from "next/head"
 
+import { Panel } from "../components"
+
 import styles from "./not-supported.module.scss"
 
 const NotSupported: PageFC = () => {
@@ -14,16 +16,32 @@ const NotSupported: PageFC = () => {
       <Head>
         <title>非対応ブラウザ</title>
       </Head>
-      <h1 className={styles.title}>
-        {isIe
-          ? "Internet Explorer ではご利用いただけません"
-          : "スマートフォンや小さなウィンドウではご利用いただけません"}
-      </h1>
-      <p className={styles.paragraph}>
-        {isIe
-          ? "Google Chrome / Mozilla Firefox などのモダンブラウザをご利用ください"
-          : "PC からご利用ください"}
-      </p>
+      <Panel>
+        <i className={`jam-icon jam-alert ${styles.icon}`} aria-hidden />
+        <h1 className={styles.title}>
+          {isIe ? "非対応ブラウザ" : "非対応デバイス"}
+        </h1>
+        {isIe ? (
+          <>
+            <p className={styles.paragraph}>
+              Internet Explorer ではご利用いただけません
+            </p>
+            <p className={styles.paragraph}>
+              {
+                "Google Chrome / Mozilla Firefox などのモダンブラウザをご利用ください"
+              }
+            </p>
+          </>
+        ) : (
+          <>
+            <p className={styles.paragraph}>
+              スマートフォンや小さなウィンドウではご利用いただけません
+            </p>
+            <p className={styles.paragraph}>PC からご利用ください</p>
+          </>
+        )}
+        <p className={styles.sos}>雙峰祭オンラインシステム</p>
+      </Panel>
     </div>
   )
 }
