683c85313435981b8e3378e6d2e6e1b06a34544d
Masaki Kobayashi <contact@mkobayashi.me>
2021-04-21 15:54:03 +0900

    Re-disable Webpack 5

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const { createSecureHeaders } = require("next-secure-headers")
 
 module.exports = withTM({
   reactStrictMode: true,
-  future: {
-    webpack5: true,
-  },
   async headers() {
     return [{ source: "/(.*)", headers: createSecureHeaders() }]
   },
