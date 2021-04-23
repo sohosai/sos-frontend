6deed2ef60869147c70dbfeff61b7e0af9e5af70
Masaki Kobayashi <contact@mkobayashi.me>
2021-04-24 03:45:49 +0900

    Load typedef for next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,12 @@ const withTM = require("next-transpile-modules")(["ky"])
 
 const { createSecureHeaders } = require("next-secure-headers")
 
-module.exports = withTM({
+/** @type {import('next/dist/next-server/server/config-shared').NextConfig} */
+const config = {
   reactStrictMode: true,
   async headers() {
     return [{ source: "/(.*)", headers: createSecureHeaders() }]
   },
-})
+}
+
+module.exports = withTM(config)
