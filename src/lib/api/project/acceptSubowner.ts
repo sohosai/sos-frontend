fe6a7b45f323d66665706023e5e022f0196c2542
Masaki Kobayashi <contact@mkobayashi.me>
2021-04-25 03:10:20 +0900

    Impl acceptSubowner api handler

---
+++ src/lib/api/project/acceptSubowner.ts
@@ -0,0 +1,27 @@
+import { client } from "../client"
+
+import type { PendingProjectId, Project } from "../../../types/models/project"
+
+declare namespace acceptSubowner {
+  type Props = Readonly<{
+    pendingProjectId: PendingProjectId
+    idToken: string
+  }>
+}
+
+const acceptSubowner = async ({
+  pendingProjectId,
+  idToken,
+}: acceptSubowner.Props): Promise<{
+  project: Project
+}> => {
+  return client({ idToken })
+    .post("pending-project/accept-subowner", {
+      json: {
+        pending_project_id: pendingProjectId,
+      },
+    })
+    .json()
+}
+
+export { acceptSubowner }
