922018cca3a37ad8b92330a9ba5c48c79cca6bd7
Masaki Kobayashi <contact@mkobayashi.me>
2021-04-28 23:44:37 +0900

    Add IconCatalogue story

---
+++ src/components/IconCatalogue/index.stories.tsx
@@ -0,0 +1,34 @@
+import { Story } from "@storybook/react"
+
+import { Panel } from "../"
+
+import styles from "./index.stories.module.scss"
+
+export default {
+  title: "IconCatalogue",
+  component: null,
+}
+
+const icons = ["log-in-alt", "log-out-alt", "checkbox", "user-f"]
+
+export const Index: Story = () => {
+  return (
+    <>
+      <div className={styles.top}>
+        <h1 className={styles.title}>Custom icons</h1>
+        <p className={styles.p}>
+          オリジナルの Jam icons に追加しているアイコン
+        </p>
+      </div>
+      <div className={styles.wrapper}>
+        {icons.map((icon) => (
+          <div className={styles.panelWrapper} key={icon} data-icon={icon}>
+            <Panel>
+              <i className={`jam-icons jam-${icon} ${styles.icon}`} />
+            </Panel>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
