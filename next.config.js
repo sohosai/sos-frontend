a10e1f083d76f563ef8907df0284cb78452d1a8f
Masaki Kobayashi <contact@mkobayashi.me>
2021-04-29 15:43:26 +0900

    Validate env vars in build time in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@ const withTM = require("next-transpile-modules")(["ky"])
 
 const { createSecureHeaders } = require("next-secure-headers")
 
+if (
+  !process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
+  !process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||
+  !process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ||
+  !process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+) {
+  throw new Error("Firebase config needed.")
+}
+
+if (!process.env.NEXT_PUBLIC_FRONTEND_URL) {
+  throw new Error("NEXT_PUBLIC_FRONTEND_URL env variable needed.")
+}
+
+if (!process.env.NEXT_PUBLIC_BACKEND_BASE_URL) {
+  throw new Error("NEXT_PUBLIC_BACKEND_BASE_URL env variable needed.")
+}
+
+if (!/^https?:\/\/.+\/$/.test(process.env.NEXT_PUBLIC_FRONTEND_URL)) {
+  throw new Error("NEXT_PUBLIC_FRONTEND_URL env variable has a wrong format.")
+}
+
 /** @type {import('next/dist/next-server/server/config-shared').NextConfig} */
 const config = {
   reactStrictMode: true,
