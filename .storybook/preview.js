b4788ace2cb9fc33d61a9d6fd1098e8d78f5d145
Masaki Kobayashi <contact@mkobayashi.me>
2021-05-01 16:35:51 +0900

    Add storybook.scss to override global.scss

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import "!style-loader!css-loader!normalize.css"
 import "../src/styles/globals.scss"
+import "./storybook.scss"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
