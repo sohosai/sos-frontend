d6b3d5cf3d99d7a7f72554604d57c41c96b68fde
Masaki Kobayashi <contact@mkobayashi.me>
2021-05-04 19:01:18 +0900

    Set background colors in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,4 +5,17 @@ import "./storybook.scss"
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   layout: "centered",
+  backgrounds: {
+    default: "white",
+    values: [
+      {
+        name: "gray-50",
+        value: "#f5f4f6",
+      },
+      {
+        name: "white",
+        value: "white",
+      },
+    ],
+  },
 }
