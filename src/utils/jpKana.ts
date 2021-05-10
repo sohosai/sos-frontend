07706a3fea8e09f8c868c6cca7469de827f86d5d
Masaki Kobayashi <contact@mkobayashi.me>
2021-05-11 00:52:38 +0900

    Adjust jp kana unicode range to adapt spec

--- src/utils/jpKana.ts
+++ src/utils/jpKana.ts
@@ -2,40 +2,40 @@
  * @returns boolean: true if `str` is empty string
  */
 const isKana = (str: string): boolean => {
-  return /^([ぁ-ゔ]|[ァ-ヴ])*$/.test(str)
+  return /^[\u3040-\u309F\u30A0-\u30FF\uFF65-\uFF9F\s]*$/.test(str)
 }
 
 /**
  * @returns boolean: true if `str` is empty string
  */
 const isHiragana = (str: string): boolean => {
-  return /^[ぁ-ゔ]*$/.test(str)
+  return /^[\u3040-\u309F\s]*$/.test(str)
 }
 
 /**
  * @returns boolean: true if `str` is empty string
  */
 const isKatakana = (str: string): boolean => {
-  return /^[ァ-ヴ]*$/.test(str)
+  return /^[\u30A0-\u30FF\uFF65-\uFF9F\s]*$/.test(str)
 }
 
 /**
- * @param  {string} str カタカナ [ァ-ヴ]
+ * @param  {string} str カタカナ
  * @returns string
  */
 const katakanaToHiragana = (str: string): string => {
-  return str.replace(/[\u30a1-\u30f4]/g, function (match) {
+  return str.replace(/[\u30a1-\u30f6]/g, function (match) {
     const chr = match.charCodeAt(0) - 0x60
     return String.fromCharCode(chr)
   })
 }
 
 /**
- * @param  {string} str ひらがな [ぁ-ゔ]
+ * @param  {string} str ひらがな
  * @returns string
  */
 const hiraganaToKatakana = (str: string): string => {
-  return str.replace(/[\u3041-\u3094]/g, function (match) {
+  return str.replace(/[\u3041-\u3096]/g, function (match) {
     const chr = match.charCodeAt(0) + 0x60
     return String.fromCharCode(chr)
   })
