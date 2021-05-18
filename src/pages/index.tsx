1fdce8ba391313646828b620f79a3ddb0f2e0cda
Masaki Kobayashi <contact@mkobayashi.me>
2021-05-19 07:48:10 +0900

    Add going-online announcement section to toppage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,44 @@
 import type { PageFC } from "next"
 
+import { staticPath } from "src/utils/$path"
+
+import { Button, Panel } from "src/components"
+
+import styles from "./index.module.scss"
+
 const Index: PageFC = () => {
-  return <></>
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.pageTitle}>雙峰祭オンラインシステム</h1>
+      <section className={styles.section} data-section="online-announcement">
+        <div className={styles.panelRowWrapper}>
+          <div className={styles.panelWrapper}>
+            <Panel>
+              <h2 className={styles.panelTitle}>対面開催中止のお知らせ</h2>
+              <div className={styles.onlineDescriptions}>
+                <p className={styles.onlineAnnouncementDate}>2021/05/19</p>
+                <p className={styles.panelText}>
+                  令和3年11月6日と11月7日に開催を予定しておりました学園祭について、新型コロナウイルスの感染が拡大している状況に鑑み対面開催の中止を決定いたしました。詳細については資料をご覧ください。
+                </p>
+                <p className={styles.panelText}>
+                  今後も学園祭について情報発信を行っていきますので、何卒ご理解のほどよろしくお願い申し上げます。
+                </p>
+              </div>
+              <a
+                href={staticPath.docs["雙峰祭対面開催中止のお知らせ_pdf"]}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button icon="download" kind="secondary">
+                  資料(PDF)
+                </Button>
+              </a>
+            </Panel>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 }
 Index.layout = "default"
 Index.rbpac = { type: "public" }
