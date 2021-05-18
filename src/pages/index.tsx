322c16da4a3e809f5ed00f2d788ca09edf1b6d05
Masaki Kobayashi <contact@mkobayashi.me>
2021-05-19 08:32:01 +0900

    Enrich toppage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import type { PageFC } from "next"
 
 import { staticPath } from "src/utils/$path"
 
+import { Timeline } from "react-twitter-widgets"
+
 import { Button, Panel } from "src/components"
 
 import styles from "./index.module.scss"
@@ -33,6 +35,43 @@ const Index: PageFC = () => {
                   資料(PDF)
                 </Button>
               </a>
+            </Panel>
+          </div>
+        </div>
+      </section>
+      <section className={styles.section} data-section="new-projects">
+        <h2 className={styles.sectionTitle}>企画募集</h2>
+        <div className={styles.panelRowWrapper}>
+          <div className={styles.panelWrapper}>
+            <Panel>
+              <p className={styles.panelText}>
+                現在企画募集に関するお知らせはありません
+              </p>
+            </Panel>
+          </div>
+        </div>
+      </section>
+      <section className={styles.section} data-section="timeline">
+        <h2 className={styles.sectionTitle}>お知らせ</h2>
+        <div className={styles.panelRowWrapper} data-cols="2">
+          <div className={styles.panelWrapper}>
+            <Panel>
+              <p className={styles.panelText}>現在お知らせはありません</p>
+            </Panel>
+          </div>
+          <div className={styles.panelWrapper}>
+            <Panel>
+              <Timeline
+                dataSource={{
+                  sourceType: "profile",
+                  screenName: "kikakurenrakun",
+                }}
+                options={{
+                  lang: "en",
+                  height: "600",
+                  dnt: true,
+                }}
+              />
             </Panel>
           </div>
         </div>
