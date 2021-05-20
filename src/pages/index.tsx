a52f1a2e95ee93a9089f1ab3caa9e00d7207ed7c
Masaki Kobayashi <contact@mkobayashi.me>
2021-05-20 18:22:26 +0900

    Add sohosai guidance link url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ dayjs.extend(timezone)
 import { Timeline } from "react-twitter-widgets"
 
 import { announcements } from "src/constants/announcements"
+import { STAGE_GUIDANCE_URL } from "src/constants/links"
 
 import { Button, Panel } from "src/components"
 
@@ -53,23 +54,47 @@ const Index: PageFC = () => {
         <div className={styles.panelRowWrapper}>
           <div className={styles.panelWrapper}>
             <Panel>
-              <h3 className={styles.panelTitle}>
-                オンラインステージ用募集要項
-              </h3>
-              <div className={styles.newProjectsParagraph}>
-                <p className={styles.panelText}>
-                  対面開催中止に伴う前回学園祭からの変更点等について記載されておりますので、企画応募をご検討の皆様は必ずご確認ください
-                </p>
+              <div className={styles.sectionInPanel}>
+                <h3 className={styles.panelTitle}>
+                  オンラインステージ用募集要項
+                </h3>
+                <div className={styles.newProjectsParagraph}>
+                  <p className={styles.panelText}>
+                    対面開催中止に伴う前回学園祭からの変更点等について記載されておりますので、企画応募をご検討の皆様は必ずご確認ください
+                  </p>
+                </div>
+                <a
+                  href={
+                    staticPath.docs[
+                      "オンラインステージ企画用募集要項_210519_pdf"
+                    ]
+                  }
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button icon="download">募集要項(ステージ企画)</Button>
+                </a>
               </div>
-              <a
-                href={
-                  staticPath.docs["オンラインステージ企画用募集要項_210519_pdf"]
-                }
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button icon="download">募集要項(ステージ企画)</Button>
-              </a>
+              <div className={styles.sectionInPanel}>
+                <h3 className={styles.panelTitle}>
+                  オンライステージ用雙峰祭ガイダンス
+                </h3>
+                <div className={styles.newProjectsParagraph}>
+                  <p className={styles.panelText}>
+                    前回学園祭からの変更点や特に重要な内容を簡潔にまとめた動画です
+                  </p>
+                  <p className={styles.panelText}>
+                    今年度は非常に多くの変更点がございますので、募集要項をご確認の際にぜひご一緒にご視聴ください
+                  </p>
+                </div>
+                <a
+                  href={STAGE_GUIDANCE_URL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button icon="arrow-up-right">雙峰祭ガイダンス</Button>
+                </a>
+              </div>
             </Panel>
           </div>
         </div>
