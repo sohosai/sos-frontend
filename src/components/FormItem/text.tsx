90fbfe9fc82728c11b8b6e5f9011229dd8f5918a
Masaki Kobayashi <contact@mkobayashi.me>
2021-05-23 14:04:25 +0900

    Impl some FormItem components

---
+++ src/components/FormItem/text.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react"
+
+import { UseFormRegisterReturn } from "react-hook-form"
+
+import { FormItem } from "src/types/models/form/item"
+
+import { Textarea, TextField } from "src/components"
+
+type Props = {
+  formItem: Extract<FormItem, { type: "text" }>
+  register: UseFormRegisterReturn
+  errors: Array<string | false | undefined>
+}
+
+const TextFormItem: FC<Props> = ({ formItem, register, errors }) => {
+  if (formItem.accept_multiple_lines)
+    return (
+      <Textarea
+        label={formItem.name}
+        required={formItem.is_required}
+        placeholder={formItem.placeholder}
+        description={formItem.description.split("\n")}
+        error={errors}
+        register={register}
+      />
+    )
+
+  return (
+    <TextField
+      type="text"
+      label={formItem.name}
+      required={formItem.is_required}
+      placeholder={formItem.placeholder}
+      description={formItem.description.split("\n")}
+      error={errors}
+      register={register}
+    />
+  )
+}
+
+export { TextFormItem }
