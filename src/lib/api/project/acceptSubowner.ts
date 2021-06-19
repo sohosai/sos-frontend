0e0c4c4e1ab0436071a3ce0b71f6da579a879a43
Masaki Kobayashi <contact@mkobayashi.me>
2021-06-20 03:17:21 +0900

    Refactor accept-subowner related codes for better error handling

--- src/lib/api/project/acceptSubowner.ts
+++ src/lib/api/project/acceptSubowner.ts
@@ -12,16 +12,50 @@ declare namespace acceptSubowner {
 const acceptSubowner = async ({
   pendingProjectId,
   idToken,
-}: acceptSubowner.Props): Promise<{
-  project: Project
-}> => {
-  return client({ idToken })
-    .post("project/create", {
-      json: {
-        pending_project_id: pendingProjectId,
-      },
-    })
-    .json()
+}: acceptSubowner.Props): Promise<
+  | Project
+  | {
+      errorCode:
+        | "pendingProjectNotFound"
+        | "tooManyProjects"
+        | "notAnsweredRegistrationForm"
+        | "alreadyProjectOwner"
+        | "alreadyProjectSubOwner"
+        | "alreadyPendingProjectOwner"
+        | "outOfProjectCreationPeriod"
+    }
+> => {
+  try {
+    const { project } = await client({ idToken })
+      .post("project/create", {
+        json: {
+          pending_project_id: pendingProjectId,
+        },
+      })
+      .json()
+    return project
+  } catch (err) {
+    const body = await err.response?.json()
+
+    switch (body.error?.info?.type) {
+      case "PENDING_PROJECT_NOT_FOUND":
+        return { errorCode: "pendingProjectNotFound" }
+      case "TOO_MANY_PROJECTS":
+        return { errorCode: "tooManyProjects" }
+      case "NOT_ANSWERED_REGISTRATION_FORM":
+        return { errorCode: "notAnsweredRegistrationForm" }
+      case "ALREADY_PROJECT_OWNER":
+        return { errorCode: "alreadyProjectOwner" }
+      case "ALREADY_PROJECT_SUBOWNER":
+        return { errorCode: "alreadyProjectSubOwner" }
+      case "ALREADY_PENDING_PROJECT_OWNER":
+        return { errorCode: "alreadyPendingProjectOwner" }
+      case "OUT_OF_PROJECT_CREATION_PERIOD":
+        return { errorCode: "outOfProjectCreationPeriod" }
+    }
+
+    throw body ?? err
+  }
 }
 
 export { acceptSubowner }
