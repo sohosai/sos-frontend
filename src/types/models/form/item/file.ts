74edf97692ffaffb340170c9dd6ba6e6abf313a1
Masaki Kobayashi <contact@mkobayashi.me>
2021-06-26 00:40:05 +0900

    Update file form item model

--- src/types/models/form/item/file.ts
+++ src/types/models/form/item/file.ts
@@ -2,7 +2,7 @@ import type { Mime } from "../../files/"
 
 export type FileFormItem = Readonly<{
   type: "file"
-  accepted_types?: Mime[]
+  accepted_types: Mime[] | null
   is_required: boolean
   accept_multiple_files: boolean
 }>
