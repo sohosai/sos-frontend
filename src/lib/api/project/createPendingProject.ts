65e24b493ec68848959762b22814cf604fbed788
Masaki Kobayashi <contact@mkobayashi.me>
2021-07-17 20:13:10 +0900

    WIP

--- src/lib/api/project/createPendingProject.ts
+++ src/lib/api/project/createPendingProject.ts
@@ -1,3 +1,5 @@
+import { HTTPError, TimeoutError } from "ky"
+
 import type { PendingProject } from "../../../types/models/project"
 import { client } from "../client"
 
@@ -15,19 +17,71 @@ declare namespace createPendingProject {
     >
     idToken: string
   }>
+
+  type ErrorType =
+    | {
+        errorCode: "invalidField"
+        field: string
+        error: any
+      }
+    | {
+        errorCode:
+          | "alreadyProjectOwner"
+          | "alreadyProjectSubowner"
+          | "alreadyPendingProjectOwner"
+          | "outOfProjectCreationPeriod"
+          | "duplicatedProjectAttributes"
+          | "timeout"
+          | "unknown"
+        error?: any
+      }
 }
 
 const createPendingProject = async ({
   props,
   idToken,
-}: createPendingProject.Props): Promise<{
-  pending_project: PendingProject
-}> => {
-  return client({ idToken })
-    .post("project/prepare", {
-      json: props,
-    })
-    .json()
+}: createPendingProject.Props): Promise<
+  PendingProject | createPendingProject.ErrorType
+> => {
+  try {
+    const { pending_project } = await client({ idToken })
+      .post("project/prepare", {
+        json: props,
+      })
+      .json()
+    return pending_project
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const body = await error.response.json()
+
+      switch (body.error?.info?.type) {
+        case "INVALID_FIELD":
+          return {
+            errorCode: "invalidField",
+            field: body.error?.info?.field,
+            error: body,
+          }
+        case "DUPLICATED_PROJECT_ATTRIBUTES":
+          return {
+            errorCode: "duplicatedProjectAttributes",
+            error: body,
+          }
+        case "ALREADY_PROJECT_OWNER":
+          return { errorCode: "alreadyProjectOwner", error: body }
+        case "ALREADY_PROJECT_SUBOWNER":
+          return { errorCode: "alreadyProjectSubowner", error: body }
+        case "ALREADY_PENDING_PROJECT_OWNER":
+          return { errorCode: "alreadyPendingProjectOwner", error: body }
+        case "OUT_OF_PROJECT_CREATION_PERIOD":
+          return { errorCode: "outOfProjectCreationPeriod", error: body }
+        default:
+          return { errorCode: "unknown", error: body }
+      }
+    } else if (error instanceof TimeoutError) {
+      return { errorCode: "timeout" }
+    }
+    return { errorCode: "unknown", error }
+  }
 }
 
 export { createPendingProject }
