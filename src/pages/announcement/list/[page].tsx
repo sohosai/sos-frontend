a740eddae568f67a2c5ca87079fdd27b932faeb4
Masaki Kobayashi <contact@mkobayashi.me>
2021-08-21 23:54:47 +0900

    Impl pagination navigation

--- src/pages/announcement/list/[page].tsx
+++ src/pages/announcement/list/[page].tsx
@@ -1,3 +1,4 @@
+import { UrlObject } from "url"
 import dayjs from "dayjs"
 import {
   PageFC,
@@ -6,15 +7,59 @@ import {
   GetStaticPaths,
 } from "next"
 import Link from "next/link"
+import { VFC } from "react"
 
 import styles from "./[page].module.scss"
-import { Head, Panel } from "src/components"
+import { Head, Panel, Icon } from "src/components"
 import { getAnnouncements } from "src/lib/contentful"
 
 import type { Announcement } from "src/types/models/announcement"
 import { pagesPath } from "src/utils/$path"
 
 const ANNOUNCEMENT_LIMIT = 20
+
+const PaginationLink: VFC<
+  | {
+      direction: "prev" | "next"
+      active?: false
+      href?: never
+    }
+  | {
+      direction: "prev" | "next"
+      active: true
+      href: UrlObject
+    }
+> = (props) => {
+  const Inner: VFC<{ direction: "prev" | "next" }> = ({ direction }) => (
+    <div className={styles.paginationLinkInner}>
+      {direction === "prev" ? (
+        <>
+          <Icon icon="chevron-left" className={styles.paginationLinkIcon} />
+          前へ
+        </>
+      ) : (
+        <>
+          次へ
+          <Icon icon="chevron-right" className={styles.paginationLinkIcon} />
+        </>
+      )}
+    </div>
+  )
+
+  return (
+    <>
+      {props.active ? (
+        <Link href={props.href}>
+          <a className={styles.paginationLinkAnchor} data-active={props.active}>
+            <Inner direction={props.direction} />
+          </a>
+        </Link>
+      ) : (
+        <Inner direction={props.direction} />
+      )}
+    </>
+  )
+}
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const { total } = await getAnnouncements({ limit: ANNOUNCEMENT_LIMIT })
@@ -86,6 +131,26 @@ const AnnouncementsList: PageFC<
           ))}
         </ul>
       </Panel>
+      <div className={styles.paginationNavigation}>
+        {pageIndex === 1 ? (
+          <PaginationLink direction="prev" active={false} />
+        ) : (
+          <PaginationLink
+            direction="prev"
+            active={true}
+            href={pagesPath.announcement.list._page(pageIndex - 1).$url()}
+          />
+        )}
+        {pageIndex === totalPages ? (
+          <PaginationLink direction="next" active={false} />
+        ) : (
+          <PaginationLink
+            direction="next"
+            active={true}
+            href={pagesPath.announcement.list._page(pageIndex + 1).$url()}
+          />
+        )}
+      </div>
     </div>
   )
 }
