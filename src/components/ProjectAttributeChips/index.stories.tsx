7dfebe3bdd620f7ef46f2fc5c019cfdfd873f2f0
Masaki Kobayashi <contact@mkobayashi.me>
2021-08-29 14:00:53 +0900

    Impl `<ProjectAttributeChips />` component

---
+++ src/components/ProjectAttributeChips/index.stories.tsx
@@ -0,0 +1,48 @@
+import { Story } from "@storybook/react"
+import { ProjectAttributeChips } from "."
+import { ProjectAttribute } from "src/types/models/project"
+
+export default {
+  title: ProjectAttributeChips.name,
+  component: ProjectAttributeChips,
+}
+
+export const Index: Story<{ [attribute in ProjectAttribute]: boolean }> = ({
+  academic,
+  artistic,
+  outdoor,
+  committee,
+}) => (
+  <ProjectAttributeChips
+    attributes={{ academic, artistic, outdoor, committee }}
+  />
+)
+Index.argTypes = {
+  attributes: {
+    table: { disable: true },
+  },
+  academic: {
+    control: {
+      type: "boolean",
+    },
+    defaultValue: true,
+  },
+  artistic: {
+    control: {
+      type: "boolean",
+    },
+    defaultValue: false,
+  },
+  outdoor: {
+    control: {
+      type: "boolean",
+    },
+    defaultValue: false,
+  },
+  committee: {
+    control: {
+      type: "boolean",
+    },
+    defaultValue: false,
+  },
+}
