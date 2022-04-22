b92b011d979876e5c228d4cc9adb1d19aae894df
maru2213 <25478531+maru2213@users.noreply.github.com>
2022-04-22 13:55:15 +0900

    Change project application guidelines url

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -2,4 +2,4 @@ export const SOHOSAI_COM = "https://sohosai.com/"
 export const TWITTER_KIKAKURENRAKUN = "https://twitter.com/kikakurenrakun/"
 export const INSTAGRAM = "https://www.instagram.com/sohosai/"
 export const FACEBOOK = "https://www.facebook.com/sohosai/"
-export const PROJECT_APPLICATION_GUIDELINES_URL = "https://sohosai.com/" //TODO: Replace with appropriate link location
+export const PROJECT_APPLICATION_GUIDELINES_URL = "https://sohosai.com/news#%E7%AC%AC48%E5%9B%9E%E7%AD%91%E6%B3%A2%E5%A4%A7%E5%AD%A6%E5%AD%A6%E5%9C%92%E7%A5%AD%E5%8B%9F%E9%9B%86%E8%A6%81%E9%A0%85%E3%81%AE%E5%85%AC%E9%96%8B"
