e334218447a58a55ca0d90d11db05fd7003f3a3c
maru2213 <25478531+maru2213@users.noreply.github.com>
2022-04-23 00:46:54 +0900

    Modify project creation period

--- src/constants/datetime.ts
+++ src/constants/datetime.ts
@@ -1,20 +1,18 @@
 import dayjs from "dayjs"
 import isSameOrAfter from "dayjs/plugin/isSameOrAfter"
-import isSameOrBefore from "dayjs/plugin/isSameOrBefore"
 import timezone from "dayjs/plugin/timezone"
 import utc from "dayjs/plugin/utc"
 
 dayjs.extend(utc)
 dayjs.extend(isSameOrAfter)
-dayjs.extend(isSameOrBefore)
 dayjs.extend(timezone)
 
-export const GENERAL_PROJECT_CREATION_PERIOD_STARTS_AT = dayjs.tz(
-  "2022-04-18T10:00:00",
+export const PROJECT_CREATION_PERIOD_STARTS_AT = dayjs.tz(
+  "2022-04-25T18:30:00",
   "Asia/Tokyo"
 )
-export const GENERAL_PROJECT_CREATION_PERIOD_ENDS_AT = dayjs.tz(
-  "2022-04-20T23:59:59",
+export const PROJECT_CREATION_PERIOD_ENDS_AT = dayjs.tz(
+  "2022-05-14T00:00:00",
   "Asia/Tokyo"
 )
 
@@ -28,8 +26,8 @@ const isInProjectCreationPeriod = () => {
   }
 
   return (
-    dayjs().isSameOrAfter(GENERAL_PROJECT_CREATION_PERIOD_STARTS_AT) &&
-    dayjs().isSameOrBefore(GENERAL_PROJECT_CREATION_PERIOD_ENDS_AT)
+    dayjs().isSameOrAfter(PROJECT_CREATION_PERIOD_STARTS_AT) &&
+    dayjs().isBefore(PROJECT_CREATION_PERIOD_ENDS_AT)
   )
 }
 
