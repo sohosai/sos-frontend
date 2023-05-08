60c253fb3b5546ae4f471af7200529c93524f173
raspi0124 <raspi0124@gmail.com>
2023-05-08 22:43:28 +0900

    エラーが発生していなくても謎の不具合が発生する場合が確認されたため、一時的にSentry Replayの対象を拡大

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -9,11 +9,8 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.NEXT_PUBLIC_DEPLOY_ENV,
   normalizeDepth: 10,
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
+  //エラーが発生していなくても謎の不具合が発生する場合が確認されたため、一時的に全てを対象に
+  replaysSessionSampleRate: 1.0,
   replaysOnErrorSampleRate: 1.0,
   integrations: [
     new CaptureConsole({ levels: ["warn", "error", "debug", "assert"] }),
